7bca0e8ed8d76da27b5a5a9b02bf7c4f60a8a767
Veizh <maximedurville95300@gmail.com>
2024-06-11 12:12:10 +0200

    first commit

--- app.js
+++ app.js
@@ -17,7 +17,13 @@ app.use(cors({
   methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE',"OPTIONS"], // Méthodes autorisées
   allowedHeaders: ['Content-Type'], // En-têtes autorisés
 }))
-
+app.use((req,res,next)=>{
+  res.setHeader('Access-Control-Allow-Origin', "https://space-traveller-six.vercel.app/");
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+})
 const mongoose = require("mongoose");
 mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log("connexion mongo db ok !"))
