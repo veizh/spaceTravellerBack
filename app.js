b78aa59c6b6321cb38e71e89e079caff8c60667d
Veizh <maximedurville95300@gmail.com>
2024-06-11 15:34:26 +0200

    debugging cors

--- app.js
+++ app.js
@@ -7,7 +7,11 @@ require("dotenv").config();
 const cors = require("cors");
 
 var app = express();
-app.use(cors())
+app.use(cors({
+  origin: "*",
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+  allowedHeaders: "Origin, X-Requested-With, x-access-token, role, Content, Accept, Content-Type, Authorization"
+}))
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
