1edbd6b5aaf1010f41d8c967c45e7fb75fcfd9e3
Veizh <maximedurville95300@gmail.com>
2024-06-11 12:25:03 +0200

    first commit

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(cors({
   allowedHeaders: ['Content-Type'], // En-têtes autorisés
 }))
 app.use((req,res,next)=>{
-  res.setHeader('Access-Control-Allow-Origin', "https://space-traveller-six.vercel.app/");
+  res.setHeader('Access-Control-Allow-Origin', "*/*");
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Credentials', true);
