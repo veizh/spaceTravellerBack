8dc397199739214b943a36364bc7f9d7812ddeee
Veizh <maximedurville95300@gmail.com>
2024-06-11 13:41:03 +0200

    debugging cors

--- routes/player.js
+++ routes/player.js
@@ -3,6 +3,7 @@ const player= require('../controller/player.js');
 var router = express.Router();
 router.post('/pushone',player.pushOne);
 router.get('/classement',player.getAll)
+router.get('/test',player.test)
 
 
 module.exports = router;
