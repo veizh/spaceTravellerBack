3b2899cb0e3741efc6aa87cb96b46b2f0818ecee
Veizh <maximedurville95300@gmail.com>
2024-06-11 13:10:19 +0200

    first commit

--- app.js
+++ app.js
@@ -11,6 +11,9 @@ const cors = require("cors");
 
 app.use(cors())
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 const mongoose = require("mongoose");
 mongoose
   .connect(process.env.MONGODB_URI, {
