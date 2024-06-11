28c449b4cd04bdee9c5469a7e50c08abc82b00e9
Veizh <maximedurville95300@gmail.com>
2024-06-11 12:04:40 +0200

    first commit

--- app.js
+++ app.js
@@ -8,30 +8,22 @@ require("dotenv").config();
 var app = express();
 const cors = require("cors");
 
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors({
-  origin: '*', 
-  methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE',"OPTIONS"], 
-  allowedHeaders: ['Content-Type'],
-  }))
-  app.use((req, res, next) => {
-    const origin = req.headers.origin;
-    if (allowedOrigins.includes(origin)) {
-      res.setHeader('Access-Control-Allow-Origin', origin);
-      }
-      res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-      res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-      res.setHeader('Access-Control-Allow-Credentials', true);
-      next();
-      });
-    app.use(express.json());
+  origin: '*', // Remplacez par votre origine autorisée
+  methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE',"OPTIONS"], // Méthodes autorisées
+  allowedHeaders: ['Content-Type'], // En-têtes autorisés
+}))
 
 const mongoose = require("mongoose");
 mongoose.connect(process.env.MONGODB_URI)
-.then(() => console.log("connexion mongo db ok !"))
+  .then(() => console.log("connexion mongo db ok !"))
   .catch(() => console.log("connexion mongo db failed ! "));
+
+
 app.use('/player',playerRouter)
 
 
